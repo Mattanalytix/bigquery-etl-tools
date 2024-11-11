8a94061e2ce3b19994d3734a464ccde976cf0702
matthew.henderson@jettytechnologies.com <matthew.henderson@jettytechnologies.com>
2024-11-11 15:38:50 +0000

    Added autodetect schema function

--- bigquery_etl_tools_package_tup/config.py
+++ bigquery_etl_tools_package_tup/config.py
@@ -1,6 +1,7 @@
 """Module containing configuration for file types"""
 
-from google.cloud import bigquery
+import logging
+from google.cloud import bigquery, storage
 import polars as pl
 
 
@@ -18,3 +19,17 @@ FILE_TYPE_CONFIG = {
         'bigquery_format': bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
     }
 }
+
+
+def get_source_format_from_blob(blob: storage.Blob) -> bigquery.SourceFormat:
+    """
+    Get the source format from a blob based on the type of blob
+    as defined in the FILE_TYPE_CONFIG
+    @param blob The blob to get the source format from
+    @return The bigquery source format for the file type (csv, json)
+    """
+    for k, v in FILE_TYPE_CONFIG.items():
+        if v['blob_type'] == blob.content_type:
+            logging.info("""Setting file type to %s as detected from
+                         the blob %s""", k, blob.name)
+            return v['bigquery_format']
